cfb29e31cd3cf67670e362d4b50a6b96c00a227f
defiled <sroma1463@gmail.com>
2025-11-02 11:23:19 +0500

    add: test.py

--- test.py
+++ test.py
@@ -1,2 +1,2 @@
-
-print('Hello World!')
+with open('.env') as f: d = dict(l.strip().split('=') for l in f if '=' in l)
+print(f"ПОДКЛЮЧЕНИЕ К СЕРВЕРУ Login: {d.get('LOGIN')}, Pass: {d.get('PASSWORD')}")
